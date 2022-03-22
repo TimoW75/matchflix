dfb3b1ca844ce569969f6a5d8cec8f49aefd4013
teun <teun.van.kessel@hva.nl>
2022-03-22 15:32:00 +0100

    db connectie

--- config/db.js
+++ config/db.js
@@ -1,20 +1,20 @@
-// //Require mongoose
-// const mongoose = require('mongoose');
+//Require mongoose
+const mongoose = require('mongoose');
 
 
 
-// //Connect to DB
-// const connectDB = () => {
-//     try { 
-//       mongoose.connect(process.env.CONNECTION_STRING, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true
-//       });
-//       console.log('DB - connected')
-//     } catch (err) {
-//       console.log('error occurred while tring to connect to DB', err);
-//       throw err;
-//     }
-//   };
+//Connect to DB
+const connectDB = () => {
+    try { 
+      mongoose.connect(process.env.CONNECTION_STRING, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+      console.log('DB - connected')
+    } catch (err) {
+      console.log('error occurred while tring to connect to DB', err);
+      throw err;
+    }
+  };
 
-// module.exports = connectDB
+module.exports = connectDB
