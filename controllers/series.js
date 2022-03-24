95f0902bb05089427c4035e8dadc924c2df1554b
TimoW75 <timo2003werkhoven@gmail.com>
2022-03-24 16:39:13 +0100

    Serie in Database

--- controllers/series.js
+++ controllers/series.js
@@ -1,21 +1,42 @@
 const fetch = require('node-fetch')
+const mongoose = require('mongoose')
+
+
+const serieSchema = new mongoose.Schema({  // nieuw model voor alle stijlen
+    serieName: String,  
+    serieImg: String,
+});
+
+const serieModel = mongoose.model('serie', serieSchema) // maak een model aan voor de stijlen
+
+const Flash = new serieModel({ serieName: 'The Flash' , serieImg: 'https://static.episodate.com/images/tv-show/thumbnail/35624.jpg'});
+
 
 const series = (req, res) => {
-	res.render('series',{
-        id: 49269,
-        name: 'Miraculous LadyBug',
-        permalink: 'miraculous-ladybug',
-        start_date: '2015-09-01',
-        end_date: null,
-        country: 'FR',
-        network: 'TF1',
-        status: 'Running',
-        image_thumbnail_path: 'https://static.episodate.com/images/tv-show/thumbnail/49269.jpg'
-      })
+    serieModel.find({}, async (err, serie) => {
+        await res.render('serieselect',{
+            serieLijst:serie
+        })
+    })
 };
 
 const seriesSubmit = (req, res) => {
-    res.render('series' )
+
+    let serieCheck = 0; // variable voor het bekijken of er een stijl is aangeklikt
+
+    serieModel.exists({serieName:'The Flash'}, async  (err, doc) => { //zoeken voor style urban in de database
+        const flashExist = doc; // variable aanmaken 
+        console.log(req.body.flash)
+        if(flashExist == null & req.body.flash =='on'){ // als de style nog niet in de database staat en als de checkbox aangeklikt is op submit
+            console.log('flash added')
+            await Flash.save(); // save de urban style naar de database
+            serieCheck++;
+        }else{
+            console.log('flash already in DB or not selected') // console log voor als de stijl al in de database staat of niet aangeklikt was
+        }    
+    });
+
+    res.render('serieselect' )
 }
 
 
