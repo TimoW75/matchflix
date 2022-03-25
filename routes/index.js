5482dbbf1e076992b79358240dbba90c0119b7a3
teun <teun.van.kessel@hva.nl>
2022-03-25 12:49:26 +0100

    random user match + user model

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,14 @@ const router = express.Router();
 const home  = require('./home');
 const login  = require('./login');
 const series = require('./series');
+const match = require('./match');
 
 
 
 router.use('/', home);
 router.use('/login', login);
 router.use('/series', series);
+router.use('/match', match);
 
 
 
