1388c1d9c8971c4e076537039622c30b3b331c17
Lina Parren <lina3@hotmail.nl>
2022-03-29 14:03:09 +0200

    functie bewerken werkend

--- controllers/update.js
+++ controllers/update.js
@@ -1,31 +1,52 @@
-const update = (req, res) => {
-	res.render('update');
-};
-
-const gewijzigd = (req, res) => {
-    res.render('gewijzigd');
-}
+const User = require('../models/user');
 
 let session;
 
-// --
-// router.post('/gewijzigd', (req, res) => {
-//     session = req.session;
-//     User.updateOne({
-//       gebruikersnaam: session.gebruikersnaam
-//     }, {
-//       voornaam: req.body.voornaam,
-//       achternaam: req.body.achternaam,
-//       gebruikersnaam: req.body.gebruikersnaam,
-//       email: req.body.email
-//     }).exec();
-//     session.gebruikersnaam = req.body.gebruikersnaam;
-//     res.redirect('/profiel');
-//   })
-// --
+const update = (req, res) => {
+  session = req.session;
+  if (!session.email) {
+    res.redirect('/');
+  } else {
+    User.find({
+      email: session.email
+    }).then((documents) => {
+      let name = documents.map(user => user.name);
+      let age = documents.map(user => user.age);
+      let gender = documents.map(user => user.gender);
+      let email = documents.map(user => user.email);
+      let about = documents.map(user => user.about);
+      let shows = documents.map(user => user.shows);
+      res.render('update', {
+        name: name,
+        age: age,
+        gender: gender,
+        email: email,
+        about: about,
+        shows: shows
+      })
+    });
+  }
+}
+
+
+const gewijzigd = async (req, res) => {
+  session = req.session;
+  User.updateOne({
+    email: session.email
+  }, {
+    name: req.body.name,
+    age: req.body.age,
+    gender: req.body.gender,
+    email: req.body.email,
+    about: req.body.about,
+    shows: req.body.shows
+  }).exec();
+  session.email = req.body.email;
+  res.redirect('/');
+}
 
 
 module.exports = {
-    update: update,
-    gewijzigd: gewijzigd,
-};
+  update: update,
+  gewijzigd: gewijzigd
+}
