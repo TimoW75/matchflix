cb093c2962352b219addb9b697d11f2ecb36f636
TimoW75 <timo2003werkhoven@gmail.com>
2022-04-01 12:13:53 +0200

    show series on home

--- controllers/series.js
+++ controllers/series.js
@@ -6,25 +6,15 @@ const sessions = require('express-session');
 let session ;
 
 
-// const series = (req, res) => {
-//     serieModel.find({}, async (err, serie) => {
-//         await res.render('serieselect',{
-//             serieLijst:serie
-//         })
-//     })
-// };
 
-const series = (req, res) => {
+const series = async (req, res) => {
     
-
-    // session = req.session
-    // if(!session.email){
-    //     res.redirect('/')
-    // }else{
-    //     User.update({email:session.email}, { $set: { shows: [] }}, function(err, affected){
-    //     console.log('affected: ', affected)
-    //     })
-    // }
+    session = req.session
+    if(!session.email){
+        await res.redirect('/')
+    }else{
+        User.update({email:session.email}, { $set: { shows: [] }}, function(err, affected){})
+    }
 
     fetch('https://www.episodate.com/api/most-popular?page=1')
     .then(response => response.json())
@@ -36,173 +26,159 @@ const series = (req, res) => {
 
 const seriesSubmit = async (req, res) => {
     let serieCheck = 0; // variable voor het bekijken of er een stijl is aangeklikt
-    console.log(req.body['the-flash'])
 
     session = req.session
     if(!session.email){
-        res.redirect('/')
+        await res.redirect('/')
     }else{
         User.find({
             email: session.email
         })
-        res.redirect('/' )
+        await res.redirect('/' )
     }
     console.log(session.email)
 
  
-
     if (req.body['the-flash'] == 'on'){
-	    const addShows = await User.findOneAndUpdate({email: session.email}, {
+	    const addShows =  User.findOneAndUpdate({email: session.email}, {
 		    $addToSet: {
 			    shows: 'the-flash'
 		    }}).lean().exec();
-        console.log(addShows)
     }
 
     if (req.body['game-of-thrones'] == 'on'){
-	    const addShows = await User.findOneAndUpdate({email: session.email}, {
+	    const addShows =  User.findOneAndUpdate({email: session.email}, {
 		    $addToSet: {
 			    shows: 'game-of-thrones'
 		    }}).lean().exec();
-        console.log(addShows)
     }
 
     if (req.body['arrow'] == 'on'){
-	    const addShows = await User.findOneAndUpdate({email: session.email}, {
+	    const addShows =  User.findOneAndUpdate({email: session.email}, {
 		    $addToSet: {
 			    shows: 'arrow'
 		    }}).lean().exec();
-        console.log(addShows)
     }
 
     if (req.body['lucifer'] == 'on'){
-	    const addShows = await User.findOneAndUpdate({email: session.email}, {
+	    const addShows =  User.findOneAndUpdate({email: session.email}, {
 		    $addToSet: {
 			    shows: 'lucifer'
 		    }}).lean().exec();
-        console.log(addShows)
     }
     
     if (req.body['supergirl'] == 'on'){
-	    const addShows = await User.findOneAndUpdate({email: session.email}, {
+	    const addShows =  User.findOneAndUpdate({email: session.email}, {
 		    $addToSet: {
 			    shows: 'supergirl'
 		    }}).lean().exec();
-        console.log(addShows)
     }
 
     if (req.body['dcs-legends-of-tomorrow'] == 'on'){
-	    const addShows = await User.findOneAndUpdate({email: session.email}, {
+	    const addShows =  User.findOneAndUpdate({email: session.email}, {
 		    $addToSet: {
 			    shows: 'dcs-legends-of-tomorrow'
 		    }}).lean().exec();
-        console.log(addShows)
     }
     
     if (req.body['the-walking-dead'] == 'on'){
-	    const addShows = await User.findOneAndUpdate({email: session.email}, {
+	    const addShows =  User.findOneAndUpdate({email: session.email}, {
 		    $addToSet: {
 			    shows: 'the-walking-dead'
 		    }}).lean().exec();
-        console.log(addShows)
     }
 
     if (req.body['dragon-ball-super'] == 'on'){
-	    const addShows = await User.findOneAndUpdate({email: session.email}, {
+	    const addShows =  User.findOneAndUpdate({email: session.email}, {
 		    $addToSet: {
 			    shows: 'dragon-ball-super'
 		    }}).lean().exec();
-        console.log(addShows)
     }
     
     if (req.body['montauk'] == 'on'){
-	    const addShows = await User.findOneAndUpdate({email: session.email}, {
+	    const addShows =  User.findOneAndUpdate({email: session.email}, {
 		    $addToSet: {
 			    shows: 'montauk'
 		    }}).lean().exec();
-        console.log(addShows)
     }
 
     if (req.body['boku-no-hero-academia'] == 'on'){
-	    const addShows = await User.findOneAndUpdate({email: session.email}, {
+	    const addShows =  User.findOneAndUpdate({email: session.email}, {
 		    $addToSet: {
 			    shows: 'boku-no-hero-academia'
 		    }}).lean().exec();
-        console.log(addShows)
     }
     
     if (req.body['the-100'] == 'on'){
-	    const addShows = await User.findOneAndUpdate({email: session.email}, {
+	    const addShows =  User.findOneAndUpdate({email: session.email}, {
 		    $addToSet: {
 			    shows: 'the-100'
 		    }}).lean().exec();
-        console.log(addShows)
     }
     if (req.body['sherlock'] == 'on'){
-	    const addShows = await User.findOneAndUpdate({email: session.email}, {
+	    const addShows =  User.findOneAndUpdate({email: session.email}, {
 		    $addToSet: {
 			    shows: 'sherlock'
 		    }}).lean().exec();
-        console.log(addShows)
     }
     
     if (req.body['supernatural'] == 'on'){
-	    const addShows = await User.findOneAndUpdate({email: session.email}, {
+	    const addShows =  User.findOneAndUpdate({email: session.email}, {
 		    $addToSet: {
 			    shows: 'supernatural'
 		    }}).lean().exec();
-        console.log(addShows)
     }
     
     if (req.body['the-big-bang-theory'] == 'on'){
-	    const addShows = await User.findOneAndUpdate({email: session.email}, {
+	    const addShows =  User.findOneAndUpdate({email: session.email}, {
 		    $addToSet: {
 			    shows: 'the-big-bang-theory'
 		    }}).lean().exec();
-        console.log(addShows)
     }
     
     if (req.body['marvel-s-agents-of-s-h-i-e-l-d'] == 'on'){
-	    const addShows = await User.findOneAndUpdate({email: session.email}, {
+	    const addShows =  User.findOneAndUpdate({email: session.email}, {
 		    $addToSet: {
 			    shows: 'marvel-s-agents-of-s-h-i-e-l-d'
 		    }}).lean().exec();
-        console.log(addShows)
     }
 
         
     if (req.body['daredevil'] == 'on'){
-	    const addShows = await User.findOneAndUpdate({email: session.email}, {
+	    const addShows =  User.findOneAndUpdate({email: session.email}, {
+		    $addToSet: {
+			    shows: 'daredevil'
+		    }}).lean().exec();
+    }
+
+            
+    if (req.body['vikings'] == 'on'){
+	    const addShows =  User.findOneAndUpdate({email: session.email}, {
 		    $addToSet: {
 			    shows: 'vikings'
 		    }}).lean().exec();
-        console.log(addShows)
     }
     
     if (req.body['gotham'] == 'on'){
-	    const addShows = await User.findOneAndUpdate({email: session.email}, {
+	    const addShows =  User.findOneAndUpdate({email: session.email}, {
 		    $addToSet: {
 			    shows: 'gotham'
 		    }}).lean().exec();
-        console.log(addShows)
     }
 
     if (req.body['westworld'] == 'on'){
-	    const addShows = await User.findOneAndUpdate({email: session.email}, {
+	    const addShows =  User.findOneAndUpdate({email: session.email}, {
 		    $addToSet: {
 			    shows: 'westworld'
 		    }}).lean().exec();
-        console.log(addShows)
     }
     
     if (req.body['miraculous-ladybug'] == 'on'){
-	    const addShows = await User.findOneAndUpdate({email: session.email}, {
+	    const addShows =  User.findOneAndUpdate({email: session.email}, {
 		    $addToSet: {
 			    shows: 'miraculous-ladybug'
 		    }}).lean().exec();
-        console.log(addShows)
     }
-    
 }
 
 
@@ -210,11 +186,6 @@ module.exports = {
     series: series,
     seriesSubmit: seriesSubmit,
 };
-
-
-
-
-
 
 //https://www.episodate.com/api/most-popular?page=1
 // https://www.episodate.com/api
