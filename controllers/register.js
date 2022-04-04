840a2f9b91773ce89f335d4d0a82cbb856e5c4e8
casperdennijs <casper.dennijs@outlook.com>
2022-04-04 21:32:40 +0200

    Console logs in het Engels en comments toegevoegd

--- controllers/register.js
+++ controllers/register.js
@@ -5,12 +5,15 @@ const saltRounds = 10;
 
 let session;
 
+// Renders the registration page when going to /register
 const registreren = (req, res) => {
     res.render('register');
 };
 
+// Registration function (stores everything in the database, creates new session & sends test email)
 const register = async (req, res) => {
     console.log(req.body.name);
+    // Encrypts newly made password
 	const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
     const createUser = new User({
         name: req.body.name,
@@ -19,24 +22,27 @@ const register = async (req, res) => {
         email: req.body.email,
         password: hashedPassword
     });
-
+    // Creates user and saves it in the database
     createUser.save((error) => {
         if (error) {
             console.error(error);
             return res.status(500).redirect('/register');
         } else {
-            console.log("Account aangemaakt!")
+            console.log("Registration is completed!")
             session = req.session;
             session.email = req.body.email;
-            sendEmail(req.body.email).catch(console.error);
+            sendEmail(req.body.email).catch(console.error); // <--- Here
             return res.status(200).redirect('series');
         }
     });
 };
 
+// Test email function (sends registration email when someone finishes registration to test platform Ethereal)
 async function sendEmail(email) {
+    // Creates test account that can ben used on Ethereal
     let testAccount = await nodemailer.createTestAccount();
-  
+    
+    // Settings to from where the email should be sent from (in this case to Ethereal)
     let transporter = nodemailer.createTransport({
       host: "smtp.ethereal.email",
       port: 587,
@@ -46,15 +52,17 @@ async function sendEmail(email) {
         pass: testAccount.pass,
       },
     });
-  
+    
+    // To which emailadress it should be sent and what content should contain in the email itself (since we use Ethereal it won't send real emails to users) 
     let info = await transporter.sendMail({
       from: '"Matchflix" <hello@matchflix.com>',
       to: email,
-      subject: "Registratie voltooid | Matchflix",
-      text: "Welkom bij Matchflix, " + email + "! Het registreren van je account is voltooid!",
-      html: "<b>Welkom bij Matchflix, " + email + "! Het registreren van je account is voltooid!</b>",
+      subject: "Registration successful | Matchflix",
+      text: "Welcome to Matchflix, " + email + "! Your account registration was successful!",
+      html: "<b>Welcome to Matchflix, " + email + "! Your account registration was successful!",
     });
-  
+    
+    // Few console logs provided for testing purposes, provides link to Ethereal where we can preview the email we sent
     console.log("Message sent: %s", info.messageId);
     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 }
