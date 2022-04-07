eb688fa4abcec9f935c83a99f9b709dba9389a0c
TimoW75 <timo2003werkhoven@gmail.com>
2022-04-07 21:37:29 +0200

    css series

---
+++ static/javascript/series.js
@@ -0,0 +1,30 @@
+
+const shows = document.querySelectorAll(".series")
+const checkbox = document.querySelectorAll(".checkbox")
+const msg = document.querySelector("#Filtermsg")
+let checkedStyles = 0
+
+
+shows.forEach(element => {
+    element.addEventListener('click', () => {
+        checkbox.forEach(element => {
+            if(element.checked == true){ 
+                checkedStyles++;
+            }
+            else{checkedStyles--}; // anders stijl naar beneden}
+            })
+            check();
+        })
+        console.log(checkedStyles)
+})
+
+function check (){ // check funtie
+    console.log(checkedStyles)
+    if(checkedStyles == -18){ // als er geen stijl is geselecteerd. Haal dan de classlist hidden weg
+        msg.classList.add("hidden")
+    } 
+    else{
+        msg.classList.remove("hidden")  // als er wel een stijl is geselecteerd voeg dan de classlist hidden toe
+    }   
+    checkedStyles = 0;
+}   
