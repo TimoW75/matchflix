8ba1b8662bbe68611ab517bacf98eda672e2d5a8
casperdennijs <casper.dennijs@outlook.com>
2022-04-07 21:54:13 +0200

    Aanpassingen CSS

--- routes/logout.js
+++ routes/logout.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/logout");
 
-router.post('/', controller.logout);
+router.post('/', controller.logoutFunction);
 
 module.exports = router;
